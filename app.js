e997c1705c82f9b2220fe71d1c50c36203082e3e
Kai Hilton-Jones <kaihiltonjones@github.com>
2016-02-20 23:14:19 +0000

    use params

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.use('/users', users);
 
 app.post('/', function (req, res) {
   //res.send('POST received');
-  res.send(req.body.number);
-  res.json(req.body.number);
+  res.send(req.params.number);
+  res.json(req.params.number);
 });
 
 // catch 404 and forward to error handler
