c728f18ffcfd4901993f3f0c2ee4ab1025101adb
Kai Hilton-Jones <kaihiltonjones@github.com>
2016-02-27 20:33:27 +0000

    Number not working

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ var repo = client.repo('KaiOrg/meeting-time');
 //});
 
 app.post('/', function (req, res) {
-  //res.send('POST received');
-  res.send(req.params.issue.number);
+  res.send('POST received');
+  //res.send(req.params.issue.number);
   //res.json(req.params.number);
   //console.log('POST received');
   // Create an issue in a repository
