46432de67226a3e24032d8f3640f98a941158be1
Kai Hilton-Jones <kaihiltonjones@github.com>
2016-02-27 21:07:33 +0000

    Create comment 2

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post('/', function (req, res) {
   octo.zen.read(cb);
   octo.repos('KaiOrg', 'meeting-time').fetch(cb);    // Fetch repo info
 
-  octo.repos('KaiOrg', 'meeting-time').issues(19).comments.create(body: 'Hello back to you!! :sparkles:');
+  octo.repos('KaiOrg', 'meeting-time').issues(19).comments.create('Hello back to you!! :sparkles:');
 
   //octo.me.starred('KaiOrg', 'meeting-time').add(cb); // Star a repo
   //octo.me.starred('KaiOrg', 'meeting-time').remove(cb); // Un-Star a repo
